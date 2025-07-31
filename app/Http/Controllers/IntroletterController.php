7a5b5fc02d4c87890a0d70b2c52cf6d437652ba1
Hashim Abbas <67165305+HashimAbbasJaffery@users.noreply.github.com>
2025-07-31 16:46:19 +0500

    Update IntroletterController.php

--- app/Http/Controllers/IntroletterController.php
+++ app/Http/Controllers/IntroletterController.php
@@ -13,7 +13,6 @@ class IntroletterController extends Controller
     public function invoice(Introletter $introletter) {
        $file_number = $introletter->member->file_number;
        $member = \DB::connection("old_mysql")->table("members_2")->where("file_no", $file_number)->first();
-         dd($member);
-       return view("Invoices.letter_invoice", compact("introletter"));
+       return view("Invoices.letter_invoice", compact("introletter", "member"));
     }
 }
