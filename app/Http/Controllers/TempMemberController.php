324c1c568b0149c032b0530b9482573928895322
hashimAbbasJaffery <habbas2121@outlook.com>
2025-08-28 18:18:08 +0500

    File bug fix

--- app/Http/Controllers/TempMemberController.php
+++ app/Http/Controllers/TempMemberController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class TempMemberController extends Controller
 {
     public function index() {
-        return view("members.temp");
+        return view("Members.temp");
     }
 }
