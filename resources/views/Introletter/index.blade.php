d881c3bd8ebbe751b92337079438e8979712cc43
Hashim Abbas <67165305+HashimAbbasJaffery@users.noreply.github.com>
2025-09-03 11:23:00 +0500

    Update index.blade.php

--- resources/views/Introletter/index.blade.php
+++ resources/views/Introletter/index.blade.php
@@ -10,7 +10,7 @@
     <!-- Remove everything INSIDE this div to a really blank page -->
     
     <div class="container px-6 mx-auto grid">
-        <div style="display: flex;">
+        <div style="display: flex; align-items: center;">
       <h2
         class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
       >
